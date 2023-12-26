26056d2d3b27e339daf22760e1341c7718155399
Tom Schena <gitlab.comsgogglespm.coil197@passmail.net>
2023-12-26 20:01:33 +0000

    "initial-pass"

--- provider/aws/encrypt/provider.tf
+++ provider/aws/encrypt/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.53.0"
+      version = "~> 5.0"
     }
 
     # https://letsencrypt.org/docs/client-options/ -> Terraform ACME Provider: https://registry.terraform.io/providers/vancluever/acme/latest
     acme = {
       source  = "vancluever/acme"
-      version = "~> 2.10.0"
+      version = "~> 2.19.0"
     }
   }
 }
